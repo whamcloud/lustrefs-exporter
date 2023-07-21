9ab8482b2b68c856a93bdf53458497e6ddad7b46
Raphael Druon <rdruon@ddn.com>
2023-07-21 01:41:10 -0600

    Add MDS stats

--- src/stats.rs
+++ src/stats.rs
@@ -1,5 +1,5 @@
 use crate::{LabelProm, Metric, StatsMapExt};
-use lustre_collector::{Stat, Target, TargetStat};
+use lustre_collector::{Stat, Target, TargetStat, MdsStat};
 use prometheus_exporter_base::prelude::*;
 use std::{collections::BTreeMap, ops::Deref};
 
@@ -188,5 +188,89 @@ pub fn build_stats(
         lustre_collector::TargetVariant::Ost => build_ost_stats(value, target, stats_map),
         lustre_collector::TargetVariant::Mgt => { /*TODO*/ }
         lustre_collector::TargetVariant::Mdt => build_mdt_stats(value, target, stats_map),
+    }
+}
+
+static MDS_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_stats",
+    help: "MDS mdt stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_FLD_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_fld_stats",
+    help: "MDS mdt_fld stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_IO_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_io_stats",
+    help: "MDS mdt_io stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_OUT_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_out_stats",
+    help: "MDS mdt_out stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_READPAGE_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_readpage_stats",
+    help: "MDS mdt_readpage stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_SEQM_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_seqm_stats",
+    help: "MDS mdt_seqm stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_SEQS_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_seqs_stats",
+    help: "MDS mdt_seqs stats",
+    r#type: MetricType::Gauge,
+};
+
+static MDS_SETATTR_STATS: Metric = Metric {
+    name: "lustre_mds_mdt_setattr_stats",
+    help: "MDS mdt_setattr stats",
+    r#type: MetricType::Gauge,
+};
+
+
+pub fn build_mds_stats(
+    x: MdsStat,
+    stats_map: &mut BTreeMap<&'static str, PrometheusMetric<'static>>
+) {
+    let MdsStat { param, stats } = x;
+
+    for x in stats {
+        let Stat {
+            name,
+            units,
+            samples,
+            ..
+        } = x;
+
+        let metric = match param.0.as_str() {
+            "mdt" => stats_map.get_mut_metric(MDS_STATS),
+            "mdt_fld" => stats_map.get_mut_metric(MDS_FLD_STATS),
+            "mdt_io" => stats_map.get_mut_metric(MDS_IO_STATS),
+            "mdt_out" => stats_map.get_mut_metric(MDS_OUT_STATS),
+            "mdt_readpage" => stats_map.get_mut_metric(MDS_READPAGE_STATS),
+            "mdt_seqm" => stats_map.get_mut_metric(MDS_SEQM_STATS),
+            "mdt_seqs" => stats_map.get_mut_metric(MDS_SEQS_STATS),
+            "mdt_setattr" => stats_map.get_mut_metric(MDS_SETATTR_STATS),
+            _ => continue,
+        };
+
+        let stat = PrometheusInstance::new()
+            .with_label("operation", name.as_str())
+            .with_label("units", units.as_str())
+            .with_value(samples);
+
+        metric.render_and_append_instance(&stat);
     }
 }
