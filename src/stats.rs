68caeccc1034226cbc777dbaaf9ddd2c3fe35ced
Raphael Druon <rdruon@ddn.com>
2023-08-04 02:30:33 -0600

    Add origin of the metric

--- src/stats.rs
+++ src/stats.rs
@@ -6,43 +6,45 @@ use prometheus_exporter_base::prelude::*;
 
 static READ_SAMPLES: Metric = Metric {
     name: "lustre_read_samples_total",
-    help: "Total number of reads that have been recorded.",
+    help: "Total number of reads that have been recorded. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Counter,
 };
 static READ_MIN_SIZE_BYTES: Metric = Metric {
     name: "lustre_read_minimum_size_bytes",
-    help: "The minimum read size in bytes.",
+    help: "The minimum read size in bytes. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Gauge,
 };
 static READ_MAX_SIZE_BYTES: Metric = Metric {
     name: "lustre_read_maximum_size_bytes",
-    help: "The maximum read size in bytes.",
+    help: "The maximum read size in bytes. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Gauge,
 };
 static READ_BYTES: Metric = Metric {
     name: "lustre_read_bytes_total",
-    help: "The total number of bytes that have been read.",
+    help: "The total number of bytes that have been read. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Counter,
 };
 
 static WRITE_SAMPLES: Metric = Metric {
     name: "lustre_write_samples_total",
-    help: "Total number of writes that have been recorded.",
+    help:
+        "Total number of writes that have been recorded. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Counter,
 };
 static WRITE_MIN_SIZE_BYTES: Metric = Metric {
     name: "lustre_write_minimum_size_bytes",
-    help: "The minimum write size in bytes.",
+    help: "The minimum write size in bytes. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Gauge,
 };
 static WRITE_MAX_SIZE_BYTES: Metric = Metric {
     name: "lustre_write_maximum_size_bytes",
-    help: "The maximum write size in bytes.",
+    help: "The maximum write size in bytes. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Gauge,
 };
 static WRITE_BYTES: Metric = Metric {
     name: "lustre_write_bytes_total",
-    help: "The total number of bytes that have been written.",
+    help:
+        "The total number of bytes that have been written. Originating from 'obdfilter.*OST*.stats'",
     r#type: MetricType::Counter,
 };
 
@@ -160,7 +162,7 @@ pub fn build_ost_stats(
 
 static MDT_STATS_SAMPLES: Metric = Metric {
     name: "lustre_stats_total",
-    help: "Number of operations the filesystem has performed.",
+    help: "Number of operations the filesystem has performed. Originating from 'mdt.*.md_stats'",
     r#type: MetricType::Counter,
 };
 
