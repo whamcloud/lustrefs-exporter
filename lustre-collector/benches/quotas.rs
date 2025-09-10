1e6c752410123fb8f99f1848f741563e4f51e9ab
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2025-09-10 10:48:21 -0400

    Add winnow benches

--- lustre-collector/benches/quotas.rs
+++ lustre-collector/benches/quotas.rs
@@ -1,9 +1,10 @@
 use combine::parser::EasyParser;
 use core::f64;
 use criterion::{Criterion, criterion_group, criterion_main};
-use lustre_collector::quota::parse as combine_parse;
+use lustre_collector::quota::{parse as combine_parse, w_parse as winnow_parse};
 use std::{fs::File, io::Read, time::Duration};
 use sysinfo::{Pid, ProcessExt, System, SystemExt};
+use winnow::Parser;
 
 #[derive(serde::Serialize)]
 struct BencherOutput {
@@ -131,6 +132,85 @@ fn get_memory_stats() -> (f64, f64) {
     } else {
         (0.0, 0.0)
     }
+}
+
+async fn test_winnow_with_mem(buffer: &String) -> MemoryUsage {
+    let (start_rss, start_virtual) = get_memory_stats();
+    let mut rss_values = vec![];
+    let mut virtual_values = vec![];
+
+    rss_values.push(start_rss);
+    virtual_values.push(start_virtual);
+
+    let (tx, mut rx) = tokio::sync::oneshot::channel::<()>();
+    let monitor_handle = tokio::spawn(async move {
+        let mut min_rss = start_rss;
+        let mut peak_rss = start_rss;
+        let mut min_virtual = start_virtual;
+        let mut peak_virtual = start_virtual;
+        let mut interval = tokio::time::interval(Duration::from_millis(100));
+
+        while rx.try_recv().is_err() {
+            interval.tick().await;
+
+            let (current_rss, current_virtual) = get_memory_stats();
+
+            rss_values.push(current_rss);
+            min_rss = min_rss.min(current_rss);
+            peak_rss = peak_rss.max(current_rss);
+
+            virtual_values.push(current_virtual);
+            min_virtual = min_virtual.min(current_virtual);
+            peak_virtual = peak_virtual.max(current_virtual);
+        }
+
+        (
+            rss_values,
+            min_rss,
+            peak_rss,
+            virtual_values,
+            min_virtual,
+            peak_virtual,
+        )
+    });
+
+    let needle = &mut buffer.as_str();
+    winnow_parse.parse(needle).unwrap();
+
+    tx.send(())
+        .expect("Failed to send stop signal to memory monitor");
+
+    let (rss_values, mut min_rss, mut max_rss, virtual_values, mut min_virtual, mut max_virtual) =
+        monitor_handle
+            .await
+            .expect("Failed to collect memory metrics from run.");
+
+    let (end_rss, end_virtual) = get_memory_stats();
+
+    min_rss = min_rss.min(end_rss);
+    max_rss = max_rss.max(end_rss);
+
+    min_virtual = min_virtual.min(end_virtual);
+    max_virtual = max_virtual.max(end_virtual);
+
+    let memory_usage = MemoryUsage {
+        start_rss,
+        end_rss,
+        memory_growth: end_rss - start_rss,
+        peak_over_start_rss_ratio: max_rss / start_rss,
+        avg_rss: rss_values.iter().sum::<f64>() / rss_values.len() as f64,
+        min_rss,
+        max_rss,
+        start_virtual,
+        end_virtual,
+        virtual_growth: end_virtual - start_virtual,
+        peak_over_start_virtual_ratio: max_virtual / start_virtual,
+        avg_virtual: virtual_values.iter().sum::<f64>() / virtual_values.len() as f64,
+        min_virtual,
+        max_virtual,
+    };
+
+    memory_usage
 }
 
 async fn test_combine_with_mem(buffer: &String) -> MemoryUsage {
@@ -264,6 +344,58 @@ pub fn combine_memory(c: &mut Criterion) {
     }
 }
 
+pub fn winnow_memory(c: &mut Criterion) {
+    let (tx, rx) = std::sync::mpsc::channel::<MemoryUsage>();
+
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_time()
+        .enable_io()
+        .build()
+        .expect("Failed to build tokio runtime");
+
+    let mut group = c.benchmark_group("parse_benchmarks");
+
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(60)); // Allow more time
+
+    group.bench_function("winnow_memory", |b| {
+        let mut raw = String::new();
+        File::open("benches/quotas.yml")
+            .unwrap()
+            .read_to_string(&mut raw)
+            .unwrap();
+
+        let tx = tx.clone();
+
+        b.to_async(&rt).iter(|| async {
+            let memory_usage = test_winnow_with_mem(&raw).await;
+
+            let _ = tx.send(memory_usage.clone());
+        })
+    });
+
+    group.finish();
+
+    drop(tx);
+
+    let samples = rx.iter().collect::<Vec<_>>();
+
+    if !samples.is_empty() {
+        let aggregated = aggregate_samples(&samples);
+
+        let bencher_output: BencherOutput = aggregated.into();
+
+        let serialized_metrics = serde_json::to_string_pretty(&bencher_output)
+            .expect("Failed to serialize benchmark output.");
+
+        let output = "winnow_mem_usage.json";
+        std::fs::write(output, serialized_metrics)
+            .expect(&format!("Failed to write benchmark results to `{output}`"));
+
+        println!("✅ Bencher results written to {output}");
+    }
+}
+
 pub fn combine_perf(c: &mut Criterion) {
     let mut group = c.benchmark_group("parse_benchmarks");
 
@@ -282,6 +414,26 @@ pub fn combine_perf(c: &mut Criterion) {
             while let Ok((_, e)) = combine_parse().easy_parse(needle) {
                 needle = e;
             }
+        })
+    });
+}
+
+pub fn winnow_perf(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse_benchmarks");
+
+    group.sample_size(10);
+    group.measurement_time(Duration::from_secs(60)); // Allow more time
+
+    group.bench_function("winnow_performance", |b| {
+        let mut raw = String::new();
+        File::open("benches/quotas.yml")
+            .unwrap()
+            .read_to_string(&mut raw)
+            .unwrap();
+
+        b.iter(|| {
+            let needle = &mut raw.as_str();
+            winnow_parse.parse(needle).unwrap();
         })
     });
 }
@@ -343,5 +495,11 @@ fn aggregate_samples(samples: &[MemoryUsage]) -> MemoryUsage {
     }
 }
 
-criterion_group!(benches, combine_perf, combine_memory);
+criterion_group!(
+    benches,
+    winnow_perf,
+    winnow_memory,
+    combine_perf,
+    combine_memory
+);
 criterion_main!(benches);
