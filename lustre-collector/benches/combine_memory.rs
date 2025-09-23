6bee26fae5fc299025b58d65e5f9ad7ad2b877fd
Jean-Baptiste Skutnik <jb.skutnik@gmail.com>
2025-09-23 14:58:20 +0530

    Ditch criterion in memory benchmarks

--- lustre-collector/benches/combine_memory.rs
+++ lustre-collector/benches/combine_memory.rs
@@ -1,60 +1,32 @@
 use combine::parser::EasyParser;
-use criterion::{Criterion, criterion_group, criterion_main};
 use lustre_collector::quota::parse as combine_parse;
-use memory_benchmarking::{BencherOutput, MemoryUsage, aggregate_samples, trace_memory};
-use std::{fs::File, io::Read, sync::mpsc, time::Duration};
+use memory_benchmarking::{BencherOutput, trace_memory};
+use std::time::Duration;
 
-pub fn combine_memory(c: &mut Criterion) {
-    let (tx, rx) = mpsc::channel();
+pub fn main() {
+    let buffer = std::fs::read_to_string("benches/quotas.yml").expect("Failed to read file");
+    let needle = buffer.as_str();
 
-    let mut group = c.benchmark_group("parse_benchmarks");
-
-    group.sample_size(10);
-    group.measurement_time(Duration::from_secs(90)); // Allow more time
-
-    let mut raw = String::new();
-    File::open("benches/quotas.yml")
-        .expect("Failed to open file")
-        .read_to_string(&mut raw)
-        .expect("Failed to read file");
-
-    group.bench_with_input("combine_memory", &raw, |b, input| {
-        b.iter(|| {
+    let samples: Vec<_> = (0..100)
+        .map(|_| {
             let routine = move || {
-                let mut needle = input.as_str();
+                let mut needle = needle;
                 while let Ok((_, e)) = combine_parse().easy_parse(needle) {
                     needle = e;
                 }
             };
 
-            let memory_usage: MemoryUsage = trace_memory(routine, Duration::from_millis(100))
+            trace_memory(routine, Duration::from_millis(10))
                 .as_slice()
                 .try_into()
-                .expect("Failed to extract memory usage from samples");
-            let _ = tx.send(memory_usage.clone());
+                .expect("Failed to extract memory usage from samples")
         })
-    });
+        .collect();
 
-    group.finish();
+    let bencher_output: BencherOutput = samples.as_slice().into();
 
-    drop(tx);
+    let serialized_metrics = serde_json::to_string_pretty(&bencher_output)
+        .expect("Failed to serialize benchmark output.");
 
-    let samples = rx.iter().collect::<Vec<_>>();
-
-    if !samples.is_empty() {
-        let aggregated = aggregate_samples(&samples);
-
-        let bencher_output: BencherOutput = aggregated.into();
-
-        let serialized_metrics = serde_json::to_string_pretty(&bencher_output)
-            .expect("Failed to serialize benchmark output.");
-
-        let output = "combine_mem_usage.json";
-        std::fs::write(output, serialized_metrics).expect("Failed to write benchmark results");
-
-        println!("✅ Bencher results written to {output}");
-    }
+    println!("{serialized_metrics}");
 }
-
-criterion_group!(benches, combine_memory);
-criterion_main!(benches);
