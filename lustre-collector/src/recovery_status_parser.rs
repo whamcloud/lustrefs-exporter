e42468a32d66dcb1dd136b26c51adac1f82f9437
Andy Breuhan <abreuhan@ddn.com>
2025-10-27 11:49:31 +0100

    Add recovery_status to lustrefs_exporter and
    enhance recovery status parser to include additional metrics:
    
    - RecoveryDuration
    - RecoveryTimeRemaining
    - RecoveryTotalClients

--- lustre-collector/src/recovery_status_parser.rs
+++ lustre-collector/src/recovery_status_parser.rs
@@ -71,7 +71,8 @@ where
         })
 }
 
-fn clients_line<I>(x: &'static str) -> impl Parser<I, Output = u64>
+/// Parses a client line containing the completed and a optional value for the total count (e.g., "completed: 5/10").
+fn clients_line<I>(x: &'static str) -> impl Parser<I, Output = (u64, Option<u64>)>
 where
     I: Stream<Token = char>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
@@ -83,15 +84,18 @@ where
         optional((token('/'), digits())),
         optional(newline().map(drop).or(eof())),
     )
-        .map(|(_, _, x, _, _): (_, _, u64, _, _)| x)
+        .map(|(_, _, x, y, _): (_, _, u64, Option<(_, u64)>, _)| (x, y.map(|(_, v)| v)))
 }
 
 #[derive(Debug)]
 enum RecoveryStat {
-    Status(RecoveryStatus),
     Completed(u64),
     Connected(u64),
     Evicted(u64),
+    RecoveryDuration(u64),
+    Status(RecoveryStatus),
+    TimeRemaining(u64),
+    Total(Option<u64>),
 }
 
 pub struct StatName(pub String);
@@ -105,6 +109,20 @@ where
     many1(alpha_num().or(one_of("_-".chars()))).map(StatName)
 }
 
+/// Parses all the simple recovery stats for a target, which are just plain u64 values
+fn simple_client_stat<I>(
+    name: &'static str,
+    constructor: fn(u64) -> RecoveryStat,
+) -> impl Parser<I, Output = Vec<RecoveryStat>>
+where
+    I: Stream<Token = char>,
+    I::Error: ParseError<I::Token, I::Range, I::Position>,
+{
+    clients_line(name)
+        .skip(optional(newline()))
+        .map(move |(x, _)| vec![constructor(x)])
+}
+
 fn target_recovery_stats<I>() -> impl Parser<I, Output = Vec<RecoveryStat>>
 where
     I: Stream<Token = char>,
@@ -113,22 +131,22 @@ where
     many(choice((
         status_line()
             .skip(optional(newline()))
-            .map(RecoveryStat::Status)
-            .map(Some),
-        clients_line("completed_clients")
-            .skip(optional(newline()))
-            .map(RecoveryStat::Completed)
-            .map(Some),
+            .map(|x| vec![RecoveryStat::Status(x)]),
+        simple_client_stat("recovery_duration", RecoveryStat::RecoveryDuration),
+        simple_client_stat("completed_clients", RecoveryStat::Completed),
+        simple_client_stat("time_remaining", RecoveryStat::TimeRemaining),
+        simple_client_stat("evicted_clients", RecoveryStat::Evicted),
         clients_line("connected_clients")
             .skip(optional(newline()))
-            .map(RecoveryStat::Connected)
-            .map(Some),
-        clients_line("evicted_clients")
-            .skip(optional(newline()))
-            .map(RecoveryStat::Evicted)
-            .map(Some),
+            .map(|(x, y)| {
+                let mut stats = vec![RecoveryStat::Connected(x)];
+                if let Some(total) = y {
+                    stats.push(RecoveryStat::Total(Some(total)));
+                }
+                stats
+            }),
         // This will ignore line/field we don't care
-        attempt((stat_name(), token(':'), till_newline().skip(newline()))).map(|_| None),
+        attempt((stat_name(), token(':'), till_newline().skip(newline()))).map(|_| vec![]),
     )))
     .map(|xs: Vec<_>| xs.into_iter().flatten().collect())
 }
@@ -176,6 +194,28 @@ where
                             value: *value,
                         })
                     }
+                    RecoveryStat::RecoveryDuration(value) => {
+                        TargetStats::RecoveryDuration(TargetStat {
+                            kind,
+                            param: param.clone(),
+                            target: target.clone(),
+                            value: *value,
+                        })
+                    }
+                    RecoveryStat::TimeRemaining(value) => {
+                        TargetStats::RecoveryTimeRemaining(TargetStat {
+                            kind,
+                            param: param.clone(),
+                            target: target.clone(),
+                            value: *value,
+                        })
+                    }
+                    RecoveryStat::Total(value) => TargetStats::RecoveryTotalClients(TargetStat {
+                        kind,
+                        param: param.clone(),
+                        target: target.clone(),
+                        value: value.unwrap_or(0),
+                    }),
                 })
                 .collect()
         })
@@ -186,19 +226,17 @@ where
     I: Stream<Token = char>,
     I::Error: ParseError<I::Token, I::Range, I::Position>,
 {
-    many(
-        (
-            target_status(),
-            skip_until(attempt(ost_or_mdt().map(drop)).or(eof())),
-        )
-            .map(|(x, _)| x.into_iter().map(Record::Target).collect()),
+    (
+        target_status(),
+        skip_until(attempt(ost_or_mdt().map(drop)).or(eof())),
     )
-    .map(|x: Vec<Vec<Record>>| x.into_iter().flatten().collect())
+        .map(|(x, _)| x.into_iter().map(Record::Target).collect())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::recovery_status_parser::{clients_line, parse, target_recovery_stats};
+    use crate::parser::parse;
+    use crate::recovery_status_parser::{clients_line, target_recovery_stats};
     use combine::{Parser, parser::EasyParser, stream::position};
 
     #[test]
@@ -238,11 +276,11 @@ mod tests {
     #[test]
     fn test_clients_line() {
         let result = clients_line("completed_clients").parse("completed_clients: 3/7\n");
-        assert_eq!(result, Ok((3, "")));
+        assert_eq!(result, Ok(((3, Some(7)), "")));
         let result = clients_line("connected_clients").parse("connected_clients: 3/7\n");
-        assert_eq!(result, Ok((3, "")));
+        assert_eq!(result, Ok(((3, Some(7)), "")));
         let result = clients_line("completed_clients").parse("completed_clients: 3\n");
-        assert_eq!(result, Ok((3, "")));
+        assert_eq!(result, Ok(((3, None), "")));
     }
 
     #[test]
@@ -259,7 +297,19 @@ IR: ENABLED
 
         let (records, _): (Vec<_>, _) = target_recovery_stats().parse(x).unwrap();
 
-        insta::assert_debug_snapshot!(records);
+        insta::assert_debug_snapshot!(records, @r"
+        [
+            Status(
+                Complete,
+            ),
+            RecoveryDuration(
+                150,
+            ),
+            Completed(
+                4,
+            ),
+        ]
+        ");
     }
 
     #[test]
@@ -275,6 +325,26 @@ completed_clients: 3
 
         let (records, _): (Vec<_>, _) = target_recovery_stats().parse(x).unwrap();
 
-        insta::assert_debug_snapshot!(records);
+        insta::assert_debug_snapshot!(records, @r"
+        [
+            Status(
+                Recovering,
+            ),
+            TimeRemaining(
+                119,
+            ),
+            Connected(
+                3,
+            ),
+            Total(
+                Some(
+                    7,
+                ),
+            ),
+            Completed(
+                3,
+            ),
+        ]
+        ");
     }
 }
